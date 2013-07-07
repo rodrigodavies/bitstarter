57830ddcadf6c1ae9051b1c47fe55fd1b8a17f62
Rodrigo Davies <rodrigo.davies@gmail.com>
2013-07-07 13:30:47 -0700

    Adds HW3 pt 1

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+// create buffer to read the file and convert to a string object
+var buffer = new Buffer(fs.readFileSync('index.html'));
+var text = buffer.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
